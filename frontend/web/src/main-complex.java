50241cc0a6539d77ab7c92dcacf76d0cfd73a80f
Wiytf9s1p1vlliamrwrv12enmJeter <varlaam.v597027154@rambler.ru>
2025-12-04 09:59:54 +0800

    add util-advanced.ts, main-complex.java, complex.rs

---
+++ frontend/web/src/main-complex.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\main-complex.java
+// Generated at: 2025-10-05T16:42:12.199Z
---
+++ service-boilerplate-routes/complex.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\service-boilerplate-routes\complex.rs
+// Generated at: 2025-10-05T16:42:12.185Z
